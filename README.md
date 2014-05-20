2f6286c0a5f4d3f81d9176bd949aa64f6480451b
uvcyclotron <utkarsh.nvm@gmail.com>
2014-05-20 18:44:58 +0530

    completed CLI version

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 PyLnks
 -------
 Python script with GUI frontend in wxWidgets to create symbolic links on Windows computers.
+
+
+Progress
+----------
+
++	Basic cmd line based utility -- done. somewhat tested.
++	GUI frontend. --todo
+
+
--- pylnks.py
+++ pylnks.py
@@ -1,15 +1,39 @@
 #PyLnks
-import subprocess
+import subprocess,os
 
 print "Welcome to PyLnks\n"
-print "D--Creates a directory symbolic (soft) link.  Default is a file symbolic link.\nH--Creates a hard link instead of a symbolic link. (Works for files) \nJ--Creates a Directory Junction. (For directory hard links)\n"
+print "D--Creates a directory symbolic (soft/shortcut) link. Default is a file symbolic link.\
+		\nH--Creates a hard link instead of a symbolic link. (Works for files) \
+		\nJ--Creates a Directory Junction. (For Directory Hard links)\n"
 opt=raw_input("[D]irectory Symlink or [H]ard link or Directory [J]unction?\n")
-tgt=raw_input("\nEnter path for target file\directory\n")
-lnk=raw_input("\nEnter path for link file\directory\n")
+if str(opt).upper() == 'D' or str(opt).upper() == 'J':
+	_fileOrDir = "Dir"
+	tgt=raw_input("\nEnter path for target source directory\n")
+	while not os.path.exists(str(tgt)):
+		print "Target not found! "
+		tgt=raw_input("\nPlease enter a valid target source path..\n")
+	lnk=raw_input("\nEnter path where dir link is to be created\n")
+	while os.path.exists(str(lnk)):
+		print "A dir already exists here."
+		lnk=raw_input("\nEnter a new path for link directory..\n")
+else:
+	_fileOrDir = "File"
+	tgt=raw_input("\nEnter path for target source file\n")
+	while not os.path.isfile(str(tgt)):
+		print "Target file not found."
+		tgt=raw_input("\nPlease enter a valid target filepath..\n")
+	lnk=raw_input("\nEnter path for link file with linkname\n")
+	while os.path.isfile(str(lnk)):
+		print "A file already exists here with that name."
+		lnk=raw_input("\nEnter a new path for link file..\n")
+#print os.path.exists(str(lnk))
+
+
 if (len(opt)>0):
 	param=" /"+str(opt).upper()
 else:
-	param=""
+	param=""  #--?why. just quit.
+
 callcmd="mklink"+param+" "+str(lnk)+" "+str(tgt) #uppercase not rqd, still put
 print "\nrunning.."
 y=subprocess.call(callcmd,shell=True)
@@ -17,3 +41,6 @@ if (y==0):
 	print "Success!" #0 sucess
 else:
 	print "Error occurred"
+
+
+
